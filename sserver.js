ef6d639380e40ba7299acdbc0b4b05b9e3994c37
Maddyahamco00 <maddyahamco00@gmail.com>
2025-01-08 21:56:08 +0100

    Update sserver.js

--- sserver.js
+++ sserver.js
@@ -13,7 +13,7 @@ app.use(bodyParser.json());
 const db = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: '1234567890#',
+    password: '   ',
     database: 'newdonation_app'
 });
 
